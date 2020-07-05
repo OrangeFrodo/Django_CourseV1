07affde01b6d5182561f3f127617de04709ebfec
OrangeFrodo <daxoja@gmail.com>
2020-07-06 00:21:07 +0200

    Update "Dynamic URL"

--- News/views.py
+++ News/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
 
-def home_view(request, *args, **kwargs):
-    print(args, kwargs)
+def home_view(request, *args, **kwargs):   
     return HttpResponse("<h1>Hello</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id} </h1>")
+
+    data = {
+        "id": tweet_id
+    }
+    status = 200
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        data["message"] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} </h1>")
