23da919d0023202f6a458e76a7db38294d8fb6ff
OrangeFrodo <daxoja@gmail.com>
2020-07-07 12:08:30 +0200

    Update "Tweets to HTML via JS"

--- CityCube/settings.py
+++ CityCube/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+#Manage.py level
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'CityCube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- News/models.py
+++ News/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Tweet(models.Model):
+class Tweet (models.Model):
     # id = models.AutoField(primary_keys = True)
     content = models.TextField(blank=True, null=True)
-    image = models.FileField(upload_to="images/", blank=True, null=True)
