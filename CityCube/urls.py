caa7f86131d283dad0d232c4bd3df3255719e653
OrangeFrodo <daxoja@gmail.com>
2020-07-14 17:01:07 +0200

    Update "TWEETS DONE"

--- CityCube/urls.py
+++ CityCube/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path("tweet-create", tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
-    path('api/tweets/', include('News.urls'))
+    path('api/tweet', include('News.urls'))
 ]
