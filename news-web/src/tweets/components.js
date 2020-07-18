960cf89d1df1ad6068424510271455d3e25ccad7
OrangeFrodo <daxoja@gmail.com>
2020-07-18 11:17:49 +0200

    Update before "gitignore"

--- news-web/src/tweets/components.js
+++ news-web/src/tweets/components.js
@@ -2,40 +2,81 @@ import React, { useEffect, useState } from 'react'
 
 import {loadTweets} from "../lookup"
 
+export function TweetsComponent(props) {
+  const textAreaRef = React.createRef()
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newVal = textAreaRef.current.value
+    console.log(newVal)
+    textAreaRef.current.value = ""
+  }
+  return <div className={props.className}>
+          <div className='col-12 mb-3'>
+            <form onSubmit={handleSubmit}>
+              <textarea ref={textAreaRef} required={true} className = 'form-control' name='tweet'>
+
+              </textarea>
+              <button type='submit' className='btn btn-primary my-3'>Tweet</button>
+            </form>
+            </div>
+        <TweetsList/>
+    </div> 
+}
+
 export function TweetsList(props){
-    const [tweets, setTweets] = useState([])
+    const [tweetsInit, setTweetsInit] = useState([])
   
     useEffect(() => {
       const myCallback = (response, status) => {
         if (status === 200){
-          setTweets(response)
+          setTweetsInit(response)
         } else {
           alert("There was an error")
         }
       }
       loadTweets(myCallback)
     }, [])
-    return tweets.map((item, index)=>{
+    return tweetsInit.map((item, index)=>{
       return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
     })
   }
 
 export function ActionBtn(props) {
     const {tweet, action} = props
+    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
+    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
     const className = props.className ? props.className : 'btn btn-primary'
-    return action.type === "like" ? <button className={className}>{tweet.likes}Likes</button> : null
+    const actionDisplay = action.display ? action.display : "Action"
+    
+    const handleClick = (event) => {
+      event.preventDefault()
+      if (action.type = "like") {
+        if (userLike === true) {
+          setLikes(likes -+ 1)
+          setUserLike(false)
+        } else {
+          setLikes(tweet.likes + 1)
+          setUserLike(true)
+        }
+
+      }
+    }
+    const display = action.type === "like" ? `${likes} ${action.display}` : actionDisplay
+    return <button className={className} onClick={handleClick}>{display}</button>
   }
   
 export function Tweet(props) {
     const {tweet} = props
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-    const like = {type:"like"}
-    const unlike = {type:"like"}
-    const retweet = {type:"like"}
+    const like = {type:"like", display:"Likes"}
+    const unlike = {type:"unlike", display:"Unlike"}
+    const retweet = {type:"retweet", display:"Retweet"}
     return <div className={className}>
         <p>{tweet.id} - {tweet.content}</p>
         <div className = 'btn btn-group'>
           <ActionBtn tweet={tweet} action={like}/>
+          <ActionBtn tweet={tweet} action={unlike}/>
+          <ActionBtn tweet={tweet} action={retweet}/>
         </div>
     </div>
   }
